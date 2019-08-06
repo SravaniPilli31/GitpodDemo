bdd9800ab227719068046841f77e0e032681deda
spilli <spilli@EWL-106820.na.leapfrog.com>
2019-08-06 14:14:39 -0700

    added testng.xml and build to pom.xml

--- Gitpod/src/test/java/LaunchBrowser.java
+++ Gitpod/src/test/java/LaunchBrowser.java
@@ -1,12 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 
 public class LaunchBrowser {
 	
-	@BeforeMethod
+	@Test
 	public void getConection(){
+//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("test-type");
+//		capabilities.setCapability("chrome.binary", "<Path to binary>");
+//		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 		System.out.println("Step 1");
 	WebDriver driver = new ChromeDriver();
 	System.out.println("Step 2");
